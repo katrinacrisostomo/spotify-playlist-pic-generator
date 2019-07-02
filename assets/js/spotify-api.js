7fbd67fc1b8e088194fba1130d640ca50be3e4fa
George Ke <georgekeee@gmail.com>
2019-07-01 23:25:49 -0400

    integrate onsubmit

--- assets/js/spotify-api.js
+++ assets/js/spotify-api.js
@@ -9,14 +9,13 @@ function redirect(url, queryParams) {
   location.href = url;
 }
 
-function generatePlaylistCover() {
+function generatePlaylistCover(e) {
   const CLIENT_ID = "326af36f3fbd4a0dbcac034ee7216a2d";
   
+  e.preventDefault();
   redirect(`https://accounts.spotify.com/authorize`, queryParams={
     client_id: CLIENT_ID,
     response_type: 'token',
     redirect_uri: 'https://google.com',
   });
 }
-//<script src="spotify-api.js"></script>
-generatePlaylistCover();
