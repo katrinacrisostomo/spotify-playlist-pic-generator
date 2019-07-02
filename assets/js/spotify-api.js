7c15fbb72bec79b93b0c540334901c94afbf9b51
George Ke <georgekeee@gmail.com>
2019-07-01 23:54:48 -0400

    Redirect to localhost

--- assets/js/spotify-api.js
+++ assets/js/spotify-api.js
@@ -10,12 +10,19 @@ function redirect(url, queryParams) {
 }
 
 function generatePlaylistCover(e) {
-  const CLIENT_ID = "326af36f3fbd4a0dbcac034ee7216a2d";
-  
   e.preventDefault();
+
+  const CLIENT_ID = "326af36f3fbd4a0dbcac034ee7216a2d";
+  const IS_DEV = true;
+  
+  // TODO: update this to the real URL once we launch this
+  let redirect_uri = "";
+  if (IS_DEV) {
+    redirect_uri = "http://localhost:8000";
+  }
   redirect(`https://accounts.spotify.com/authorize`, queryParams={
     client_id: CLIENT_ID,
     response_type: 'token',
-    redirect_uri: 'https://google.com',
+    redirect_uri: redirect_uri,
   });
 }
