67d78e08f64759d8ae62a8b6cf74d19796d984cc
George Ke <georgekeee@gmail.com>
2019-07-02 21:09:31 -0400

    use single quotes, note use unnecessary backticks

--- assets/js/spotify-api.js
+++ assets/js/spotify-api.js
@@ -1,6 +1,6 @@
 function redirect(url, queryParams) {
   if (queryParams) {
-    let queryString = "?";
+    let queryString = '?';
     for (const key in queryParams) {
       queryString += `${key}=${queryParams[key]}&`
     }
@@ -10,10 +10,10 @@ function redirect(url, queryParams) {
 }
 
 function generatePlaylistCover(e) {
-  const CLIENT_ID = "326af36f3fbd4a0dbcac034ee7216a2d";
+  const CLIENT_ID = '326af36f3fbd4a0dbcac034ee7216a2d';
   
   e.preventDefault();
-  redirect(`https://accounts.spotify.com/authorize`, queryParams={
+  redirect('https://accounts.spotify.com/authorize', queryParams={
     client_id: CLIENT_ID,
     response_type: 'token',
     redirect_uri: 'https://google.com',
